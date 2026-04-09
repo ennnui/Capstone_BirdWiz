22a008c712e4c1bbc895a94a8bb81677c0a396ec
ohwellelle <eleung2@sva.edu>
2026-04-09 14:08:34 -0400

    fixed teeth decals

--- potion.gd
+++ potion.gd
@@ -5,9 +5,16 @@ var speed = 600
 
 func _physics_process(delta): 
 	#look_at(get_global_mouse_position())
-	position += speed * transform.x* delta
-	if area.has_overlapping_areas():
-		queue_free()
+	position += speed * transform.x * delta
+	#if area.has_overlapping_areas():
+		#queue_free()
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area)
+	if area.is_in_group("curse"):
+		area.queue_free()
+		queue_free()
