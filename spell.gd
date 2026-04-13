860364db4f8e443eef2977afc854fae2c0387496
ennnui <mblokhina@sva.edu>
2026-04-13 17:32:36 -0400

    spell animation art done
    and it works

---
+++ spell.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var speed = 600
+@onready var area = $Area2D
+
+func _physics_process(delta): 
+	#look_at(get_global_mouse_position())
+	position += speed * transform.x * delta
+	#if area.has_overlapping_areas():
+		#queue_free()
+	$AnimationPlayer.play("spell_animation")
+
+func _on_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area)
+	if area.is_in_group("curse"):
+		area.queue_free()
+		queue_free()
