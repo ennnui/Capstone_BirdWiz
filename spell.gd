d76bae33a091ef8e6be5fa48474ac29816c29dfa
ennnui <mblokhina@sva.edu>
2026-05-03 14:06:16 -0400

    Blur started

--- spell.gd
+++ spell.gd
@@ -17,6 +17,6 @@ func _on_timer_timeout() -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print(area)
-	if area.is_in_group("curse"):
+	if area.is_in_group("curse") or area.is_in_group("bad_curse"):
 		area.queue_free()
 		queue_free()
