64f6adce24128d6ca174e31a8fbf03fa5471e04e
ohwellelle <eleung2@sva.edu>
2026-05-03 18:32:49 -0400

    push

--- vision_curse.gd
+++ vision_curse.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 var triggered = false 
+@onready var area = $Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Area2D" and not triggered:
 		triggered = true 
+		
