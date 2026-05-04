b568a72e754da01e293a21b06ec7cc0176e68472
ennnui <mblokhina@sva.edu>
2026-05-04 05:31:46 -0400

    SCROLL COMPLETE
    will i remember this moment when i'm old and frail? is anyone reading these?

--- scroll_large.gd
+++ scroll_large.gd
@@ -1,12 +1,19 @@
 extends Area2D
 
+var triggered = false
+
 func _ready():
-	print_tree_pretty()
+	#print_tree_pretty()
 	$"../scroll_large".visible = false  # hidden on load
+	$"../scroll_large/instructions".visible = false
 
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D":
+	if body.name == "CharacterBody2D" and not triggered:
+		triggered = true
 		$"../scroll_large".visible = true
+		$AnimatedSprite2D.play("open_sesame_large")
+		await get_tree().create_timer(1.0).timeout
+		$"../scroll_large/instructions".visible = true
 		
 		
 		#print_tree_pretty()
